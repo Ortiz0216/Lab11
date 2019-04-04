130e97b028242ea60b56a6251c50ff75b5afdcad
Daniel Ortiz <do973658@wtstudents.wtamu.edu>
2019-04-04 12:10:07 -0500

    First Commit

---
+++ Models/Professor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab11.Models
+{
+    public class Professor
+    {
+
+        public int ID {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+
+    }
+
+}
